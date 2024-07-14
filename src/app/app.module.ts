c7aba19e1cc227acb7ac6b112264e2cdb3ff3381
gearsandcode <gearsandcode@users.noreply.github.com>
2024-07-14 15:53:51 -0700

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { ThemeModule } from './theme/theme.module';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, ThemeModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, ThemeModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
